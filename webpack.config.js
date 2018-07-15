a2e2f4833a9faf3c03914a06055bcc9bd65cb6b3
Jeff Lewis <jeffylew77@gmail.com>
2018-07-14 18:27:06 -0700

    [Dev] Adding Webpack config

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+// Imports
+const PATH = require('path');
+const HTMLWEBPACKPLUGIN = require('html-webpack-plugin');
+
+
+// Webpack Configuration
+const CONFIG = {
+  // Entry
+  entry: './client/src/index.jsx',
+  // Output
+  output: {
+    path: PATH.join(__dirname, './client/dist'),
+    filename: 'bundle.js'
+  },
+  // Loaders
+  module: {
+    rules : [
+      // JavaScript/JSX Files
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      // CSS Files
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      // Image Files
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        use: 'file-loader'
+      }
+    ]
+  },
+  // Plugins
+  plugins: [
+    new HTMLWEBPACKPLUGIN({
+      template: './client/src/index.html'
+    })
+  ]
+};
+
+// Exports
+module.exports = CONFIG;
