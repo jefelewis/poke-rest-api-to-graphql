22c9d6923f09aec58873fa2084e8c44508c15107
Jeff Lewis <jeffylew77@gmail.com>
2018-07-19 16:21:57 -0700

    [Dev] Adding error handling to axios get request

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -8,10 +8,18 @@ const RESOLVERS = {
     getPokemonByID: (parent, args) => {
       return axios.get(`http://pokeapi.co/api/v2/pokemon/${args.id}`)
       .then(response => response.data)
+      .catch(function (error) {
+        // Handle Error
+        console.log(error);
+      })
     },
     getAllPokemon:  (parent, args) => {
       return axios.get(`https://pokeapi.co/api/v2/pokemon/?limit=20`)
       .then(response => response.data)
+      .catch(function (error) {
+        // Handle Error
+        console.log(error);
+      })
     }
   }
 };
